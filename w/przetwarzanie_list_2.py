fef33dee3347028034f103986d30312ed65c5249
mkorczynska <k.martyna95@gmail.com>
2019-12-14 00:44:44 +0100

    .

--- w/przetwarzanie_list_2.py
+++ w/przetwarzanie_list_2.py
@@ -1,9 +1,11 @@
-from functools import reduce
-
 names = ['Jan', 'Adam', 'Karol', 'Jakub', 'Edmund']
 hours = [168, 176, 142, 154, 120]
 h_wage = [30, 22, 28, 34, 58]
 
-result = [x for x in list(zip(names, list(map(lambda x, y: x * y, hours, h_wage))))
-          if x[1] > (reduce(lambda x, y: x + y, x)) / len(x)]
-print(result)
+result = [x for x in list(zip(names, list(map(lambda x, y: x * y, hours, h_wage))))]
+payments = list(map(lambda x: x[0]*x[1], zip(hours, h_wage)))
+mean = sum(payments)/len(payments)
+
+for i in range(len(payments)):
+    if payments[i] > mean:
+        print(result[i])
