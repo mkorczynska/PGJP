117163ebf31caac77dc88f17ce6019eca8ee947f
mkorczynska <k.martyna95@gmail.com>
2019-12-16 23:52:49 +0100

    .

--- cw_3/komiwojazer.py
+++ cw_3/komiwojazer.py
@@ -1,5 +1,6 @@
 import numpy
 import matplotlib.pyplot as plt
+import random as rd
 
 towns = [(30, 70), (50, 60), (30, 50), (0, 40), (50, 20), (30, 0), (30, 20), (40, 30), (60, 30)]
 
@@ -29,32 +30,77 @@ m = cartesian_matrix(towns)
 t = [0, 8, 3, 2, 1, 5, 6, 7, 4]
 print(t, "%8.3f" % tour_length(m, t))
 
-# wspx, wspy = zip(*towns)
-# plt.scatter(wspx, wspy)
-# for j in range(len(t)):
-#     plt.annotate(j, towns[t[j]])
-# plt.show()
-#
-# lines = plt.plot((x1, x2), (y1, y2))
-# plt.setp(lines, color='r', linewidth=1.5)
-#
-# no_first_pop = 16
-# new_population = []
-#
-# for i in range(0, no_first_pop):
-#     rd.shuffle(t)
-#     ax = list(t)
-#     new_population.append(ax)
-#
-#
-# def choice_for_crossover(pop, no_crossover):
-#     pop_chosen = []
-#     len_tours = [tour_length(m, os) for os in pop]
-#     for j in range(0, len(pop)):
-#         max_fitness_idx = len_tours.index(min(len_tours))
-#         pop_chosen.append(pop[max_fitness_idx])
-#         len_tours[max_fitness_idx] = 9999999999
-#     return pop_chosen[:no_crossover]
-#
-#
-# chosen_pop = choice_for_crossover(new_population, no_for_crossover)
+wspx, wspy = zip(*towns)
+plt.scatter(wspx, wspy)
+for j in range(len(t)):
+    plt.annotate(j, towns[t[j]])
+plt.show()
+
+lines = plt.plot((x1, x2), (y1, y2))
+plt.setp(lines, color='r', linewidth=1.5)
+
+no_first_pop = 16
+new_population = []
+
+for i in range(0, no_first_pop):
+    rd.shuffle(t)
+    ax = list(t)
+    new_population.append(ax)
+
+
+def choice_for_crossover(pop, no_crossover):
+    pop_chosen = []
+    len_tours = [tour_length(m, os) for os in pop]
+    for j in range(0, len(pop)):
+        max_fitness_idx = len_tours.index(min(len_tours))
+        pop_chosen.append(pop[max_fitness_idx])
+        len_tours[max_fitness_idx] = 9999999999
+    return pop_chosen[:no_crossover]
+
+
+chosen_pop = choice_for_crossover(new_population, no_for_crossover)
+
+def crossover(parents, offspring_size):
+    offspring=[]
+    for i in range(0, offspring_size):
+        chrom = [0 for j in range(0, num_towns)]
+        offspring.append(chrom)
+    for k in range(0, int(offspring_size/2)):
+        p_1 = rd.randint(0, no_for_crossover-1)
+        p_2 = rd.randint(0, no_for_crossover-1)
+        c_point = rd.randint(0, len(parents[1]))
+        offspring[k] = parents[p_1][:c_point]
+        offspr_1 = [x for x in parents[p_2] if not x in offspring[k]]
+        offspring[k] += offspr_1
+        offspring[offspring_size-k-1] = parents[p_2][:c_point]
+        offspr_2 = [x for x in parents[p_1] if not x in offspring[offspring_size-k-1]]
+        offspring[offspring_size-k-1] += offspr_2
+    return offspring
+
+
+def mutation(offspring_crossover, p_mute, offspring_size):
+    for idx in range(0, offspring_size):
+        pr = rd.uniform(0, 1)
+        if pr < p_mute:
+            m_point1 = rd.randint(0, num_towns - 1)
+            m_point2 = rd.randint(0, num_towns - 1)
+            pom = offspring_crossover[idx][m_point1]
+            offspring_crossover[idx][m_point1] = offspring_crossover[idx][m_point2]
+            offspring_crossover[idx][m_point2] = pom
+        return offspring_crossover
+
+print(" -----------pop--------------")
+for i in range(0, no_first_pop):
+    print(new_population[i], "%8.3f" % tour_length(m, new_population[i]))
+print(" -----------pop-chosen--------")
+chosen_pop = choice_for_crossover(new_population, no_for_crossover)
+for i in range(0, no_for_crossover):
+    print(chosen_pop[i], "%8.3f" % tour_length(m, chosen_pop[i]))
+off = crossover(chosen_pop, no_for_crossover)
+print("------potomkowie---------")
+for i in range(0, no_for_crossover)
+    print(off[i], "%8.3f" % tour_length(m, off[i]))
+off = mutation(off, p_mute, no_for_crossover)
+print("----po mutacji----")
+for i in range(0, no_for_crossover):
+    print(off[i], "%8.3f" % tour_length(m, off[i]))
---
+++ cw_4/zadanie_graficzne.py
@@ -0,0 +1,122 @@
+import random as rm
+from PIL import Image, ImageDraw
+
+# ---------------------
+n = 44
+dl_genu = 15
+szer_mapy = 128
+wys_mapy = 128
+pop = []
+p_mute1 = 0.18
+p_mute2 = 0.12
+# ---------------------
+
+im = Image.open('gen_1.png')
+im.save('gen_copy.png')
+im.close()
+im = Image.open('gen_copy.png')
+
+rm.seed(72)
+for i in range(0, n):
+    cele = [[rm.randint(0, szer_mapy), rm.randint(0, wys_mapy)] for j in range(0, dl_genu)]
+    pop += [cele]
+for i in range(0, n):
+    print(pop[i])
+
+
+# i = 0
+# obr = ImageDraw.Draw(im)
+# for j in range(0, dl_genu):
+#     obr.ellipse([pop[i][j][0] - 10, pop[i][j][1] - 10, pop[i][j][0] + 10, pop[i][j][1] + 10], fill='red', outline='red')
+# im.load()
+# im.show()
+#
+# k = int(im.getcolors()[-1][0])
+# im.close()
+# print(k)
+
+
+def zliczanie_czarnych(popul, l_os):
+    eval_l = []
+    for i in range(0, l_os):
+        img = Image.open('gen_1.png')
+        img_d = ImageDraw.Draw(img)
+        for j in range(0, dl_genu):
+            img_d.ellipse([popul[i][j][0] - 10, popul[i][j][1] - 10, popul[i][j][0] + 10, popul[i][j][1] + 10],
+                          fill='red', outline='red')
+        img.load()
+        z = int(img.getcolors()[-1][0])
+        img.close()
+        eval_l += [z]
+    return eval_l
+
+
+czarne = zliczanie_czarnych(pop, n)
+print("Zliczone czarne: \n", czarne)
+
+r1 = rm.randint(0, n - 1)
+r2 = rm.randint(0, n - 1)
+
+os1 = pop[r1] if czarne[r1] <= czarne[r2] else pop[r2]
+
+r1 = rm.randint(0, n - 1)
+r2 = rm.randint(0, n - 1)
+
+os2 = pop[r1] if czarne[r1] <= czarne[r2] else pop[r2]
+
+s = [rm.randint(0, szer_mapy), rm.randint(0, wys_mapy)]
+print("Srodek: ", s)
+
+r = rm.randint(0, wys_mapy)
+print("Promien: ", r)
+
+wew_os1 = [x for x in os1 if abs(x[0] - s[0]) + abs(x[1] - s[1]) <= r]
+zew_os1 = [x for x in os1 if abs(x[0] - s[0]) + abs(x[1] - s[1]) >= r]
+print('wew_os2', wew_os1, 'zew_os2', zew_os1)
+
+wew_os2 = [x for x in os2 if abs(x[0] - s[0]) + abs(x[1] - s[1]) <= r]
+zew_os2 = [x for x in os2 if abs(x[0] - s[0]) + abs(x[1] - s[1]) >= r]
+print('wew_os2', wew_os2, 'zew_os2', zew_os2)
+
+nowy1 = wew_os1 + zew_os2
+nowy2 = wew_os2 + zew_os1
+
+while len(nowy1) > dl_genu:
+    del nowy1[rm.randint(0, dl_genu - 1)]
+
+while len(nowy2) < dl_genu:
+    i = rm.randint(0, len(wew_os1) - 1)
+    if not wew_os1[i] in nowy2:
+        nowy2 += [wew_os1[i]]
+
+
+def mut(gen, dl):
+    gen[rm.randint(0, dl - 1)] = [rm.randint(0, 128), rm.randint(0, 128)]
+    return gen
+
+
+def step(gen, dl):
+    i, j = rm.randint(0, dl - 1), rm.randint(0, dl - 1)
+
+    gen[i][0] += int((rm.random() - 0.5) * 0.25 * 5)
+    gen[j][0] += int((rm.random() - 0.5) * 0.25 * 5)
+
+    if gen[i][0] < 0:
+        gen[i][0] = 0
+    elif gen[i][0] > 128:
+        gen[i][0] = 128
+    if gen[j][0] < 0:
+        gen[j][0] = 0
+    elif gen[j][0] < 128:
+        gen[j][0] = 128
+    return gen
+
+
+if rm.random() < p_mute1: nowy1 = mut(nowy1, dl_genu)
+if rm.random() < p_mute2: nowy1 = step(nowy1, dl_genu)
+if rm.random() < p_mute1: nowy2 = mut(nowy2, dl_genu)
+if rm.random() < p_mute2: nowy2 = step(nowy2, dl_genu)
+potomek = []
+potomek += [nowy1, nowy2]
+
+print(potomek)
