06bb1b799163eb1c6f5710c4c9cd5dba77b82d64
mkorczynska <k.martyna95@gmail.com>
2020-01-22 13:47:37 +0100

    .

--- w/parking.py
+++ w/parking.py
@@ -1,80 +1,73 @@
-# %%writefile parking2.py
-# imporujemy shelve - do obslugi bazy
-# sys - do wychodzeniania z programu
-# time - do obslugi czasu
-import shelve, sys
+import shelve
+import sys
 from time import *
 
 
 def sprzedaz(godz, rej):
     global baza
-    abonament = input("Czy chcesz wykupic abonament <Y/N>: ").upper()
-    if abonament == 'Y':
+    abonament = input("Czy chcesz wykupic abonament <T/N>: ").upper()
+    if abonament == 'T':
         baza[rej] = baza[rej][0], godz
     else:
         baza[rej] = baza[rej][0], 'brak'
 
 
-# zmiana oplat
 def zmiana_stawki():
-    global baza, stawka, okres  # zmienne globalne
+    global baza, stawka, okres
     print("\nZmiana wysokości opłat\n")
     print("Bieżąca stawka wynosi %.2f zł za %i minut(y)\n" % (stawka, okres))
     try:
         s = float(input("Podaj nową wysokość opłat: "))
         o = int(input("Podaj nowy czas naliczania w minutach: "))
     except:
-        print("Błąd wprowadzania danych! Stawka nie została zmieniona!")
+        print("Błąd! Stawka nie została zmieniona!")
         return
     try:
-        baza['_stawka'] = (s, o)  # zapisujemy w bazie
+        baza['_stawka'] = (s, o)
     except:
-        print("Błąd zapisu danych! Stawka nie została zmieniona!")
+        print("Błąd! Stawka nie została zmieniona!")
     else:
-        stawka = s  # kopiujemy do
-        okres = o  # zmiennych globalnych
+        stawka = s
+        okres = o
 
 
-# inicjaliazcja bazy danych
 def init():
     global baza, stawka, okres
     try:
-        baza = shelve.open('baza_parkingowa_2')  # otwarcie
+        baza = shelve.open('baza_parkingowa_2')
     except:
-        print("Błąd krytyczny! Baza danych nie została otwarta!")
-        sys.exit(0)  # wyjście z programu
-    print("Inicjalizacja udana. Baza danych została otwarta.")
-    if '_stawka' in baza.keys():  # czy już istnieje?
-        (stawka, okres) = baza['_stawka']  # tak - kopiujemy stawki
+        print("Błąd! Baza danych nie została otwarta!")
+        sys.exit(0)
+    print("Baza danych została otwarta.")
+    if '_stawka' in baza.keys():
+        (stawka, okres) = baza['_stawka']
     else:
-        (stawka, okres) = (0.0, 1)  # nie -
-        zmiana_stawki()  # wczytujemy od użytkownika
+        (stawka, okres) = (0.0, 1)
+        zmiana_stawki()
 
 
-# menu główne programu
 def menu():
     while True:
         print()
-        print('-' * 70)
-        print('PARKING'.center(70))
-        print('-' * 70)
-        print('[W] Wjazd [E] Wyjazd [P] Pojazdy [S] Stawka [K] Koniec'.center(70))
-        print('-' * 70)
-        w = input()[0].upper()  # pierwszy znak (duża litera)
-        if w in 'WEPSK':  # znany?
-            return w  # tak - zwracamy go
+        print('_' * 100)
+        print('PARKING'.center(100))
+        print('_' * 100)
+        print('[W] Wjazd [E] Wyjazd [P] Pojazdy [S] Stawka [K] Koniec'.center(100))
+        print('_' * 100)
+        w = input()[0].upper()
+        if w in 'WEPSK':
+            return w
         print('Nieznane polecenie -')
 
 
-# lista pojazdów na parkingu
 def pojazdy():
     global baza
     print()
     print('Lista pojazdów na parkingu'.center(33))
-    print('-' * 58)
+    print('_' * 58)
     print('|' + 'Nr rej.'.center(10) + '|' + 'Godz. parkowania'.center(20) + '|' + 'Data zakupu abonamentu'.center(
         25) + '|')
-    print('-' * 58)
+    print('_' * 58)
     for rej, godz in baza.items():
         if rej != '_stawka':
             if godz[1] == 'brak':
@@ -82,15 +75,14 @@ def pojazdy():
             else:
                 print("|%9s|" % rej, strftime("%H:%M (%Y-%m-%d)", godz[0]), '|',
                       strftime("%Y-%m-%d", godz[0]).center(25), '|')
-    print('-' * 58)
+    print('_' * 58)
 
 
-# rejestracja wyjazdu pojazdu
 def wyjazd():
     global baza, stawka, okres, godz, rej, godz2
     print("Wyjazd pojazdu - godzina", strftime("%H:%M (%Y-%m-%d)"))
     rej = input("Podaj numer rejestracyjny pojazdu: ")
-    if rej in baza.keys():  # czy taki byl zaparkowany?
+    if rej in baza.keys():
         zakup_abonamentu = baza[rej][1]
         godz = baza[rej][0]
         if zakup_abonamentu != 'brak':
@@ -103,9 +95,9 @@ def wyjazd():
         if zakup_abonamentu == 'brak':
             print("Godzina wjazdu:", strftime("%H:%M (%Y-%m-%d)", godz))
             minuty = int(mktime(localtime()) - mktime(godz)) / 60
-            jednostki = (minuty + okres - 1) / okres  # naliczamy za rozpoczeta godzine
+            jednostki = (minuty + okres - 1) / okres
             print("\n Do zapłaty: %.2f zł" % (jednostki * stawka))
-            del baza[rej]  # usuwamy wpis
+            del baza[rej]
         else:
             dni = int(mktime(localtime()) - mktime(zakup_abonamentu)) // 43200
             baza[rej] = strptime("1 Jan 00", "%d %b %y"), baza[rej][1]
@@ -115,15 +107,14 @@ def wyjazd():
         print("Błąd! Takiego pojazdu nie ma na parkingu!")
 
 
-# rejestracja wjazdu pojazdu
 def wjazd():
     global baza, rej, godz
     godz = localtime()
     print("Wjazd pojadu - godzina", strftime("%H:%M (%Y-%m-%d)", godz))
     rej = input("Podaj numer rejestracyjny pojazdu: ")[:9]
     if rej == '_stawka':
-        return  # zabezpieczenie
-    if rej not in baza.keys():  # nie jest zaparkowany?
+        return
+    if rej not in baza.keys():
         baza[rej] = godz, 'brak'
         sprzedaz(godz, rej)
         print("Wprowadzono.")
@@ -134,7 +125,6 @@ def wjazd():
         print("Wprowadzono")
 
 
-# realizacja wyboru użytkownika
 def wybor():
     while True:
         w = menu()
@@ -150,10 +140,9 @@ def wybor():
             wjazd()
 
 
-#program główny
-init() #otwiercie bazy
+init()
 try:
-    wybor() #interfejs uzytkownika
+    wybor()
 except:
-    print("Wystąpił poważny błąd.")
-baza.close() #zamykanie bazy
+    print("Wystąpił błąd.")
+baza.close()
